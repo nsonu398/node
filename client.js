91b0380529c4b4310856145b82c81ce3fb0f8982
ChandraSekhar <chandrashekhar.sahu@mysalesbook.in>
2021-12-27 19:41:37 +0530

    sc

---
+++ client.js
@@ -0,0 +1,24 @@
+const { io } = require("socket.io-client");
+
+const socket = io("ws://localhost:3000");
+
+socket.on("connect", () => {
+  // either with send()
+  for(i=0;i<10000;i++){
+    socket.send(i);
+  }
+
+  // or with emit() and custom event names
+  //socket.emit("salutations", "Hello!", { "mr": "john" }, Uint8Array.from([1, 2, 3, 4]));
+});
+
+// handle the event sent with socket.send()
+socket.on("message", data => {
+    i = data;
+  console.log(data);
+});
+
+// handle the event sent with socket.emit()
+socket.on("greetings", (elem1, elem2, elem3) => {
+  console.log(elem1, elem2, elem3);
+});
